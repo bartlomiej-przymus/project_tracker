070148c247aad5f3686b3fadcd731accce9f5c1f
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2020-05-25 23:03:54 +0100

    renamed controller to make it clearer what it does

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,7 +16,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Auth');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -30,9 +30,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-$routes->match(['get','post'], 'login', 'Home::login');
-$routes->match(['get','post'], 'register', 'Home::register');
+$routes->get('/', 'Auth::index');
+$routes->match(['get','post'], 'login', 'Auth::login');
+$routes->match(['get','post'], 'register', 'Auth::register');
 //$routes->get('(:any)', 'Pages::view/$1');
 
 /**
